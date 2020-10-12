3173b62c11d53c9b293b1b1c8048c574eab561bb
Jarred Green <astrojarred@gmail.com>
2020-10-12 20:13:07 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='notion-to-doist',
+    version='0.01',
+    packages=['notion2doist'],
+    url='',
+    license='',
+    author='Jarred Green',
+    author_email='astrojarred@gmail.com',
+    description='Enables a hacked-together two-way sync between notion and todoist.'
+)
